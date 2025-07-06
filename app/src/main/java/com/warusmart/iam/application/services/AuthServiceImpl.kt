12ac49fbc1948a477a487e94b4509bc94fe02906
christopherMore1 <christopmore28@gmail.com>
2025-07-06 17:37:25 -0500

    API_URL bug fixed

--- app/src/main/java/com/warusmart/iam/application/services/AuthServiceImpl.kt
+++ app/src/main/java/com/warusmart/iam/application/services/AuthServiceImpl.kt
@@ -9,38 +9,42 @@ import com.warusmart.iam.infrastructure.AuthService
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import kotlin.math.log
 
 class AuthServiceImpl(private val authService: AuthService) {
 
     fun signUp(request: SignUpRequest, callback: (SignUpResponse?) -> Unit) {
+        Log.d("Authentication service","Called signup")
         authService.signUp(request).enqueue(object : Callback<SignUpResponse> {
             override fun onResponse(call: Call<SignUpResponse>, response: Response<SignUpResponse>) {
-                Log.d("AuthService",response.errorBody().toString())
                 if (response.isSuccessful) {
                     callback(response.body())
                 } else {
+                    Log.d("Authentication service","Failed signup")
                     callback(null)
                 }
             }
 
             override fun onFailure(call: Call<SignUpResponse>, t: Throwable) {
+                Log.d("Authentication service", "Error signin: ${t.message}")
                 callback(null)
             }
         })
     }
 
     fun signIn(request: SignInRequest, callback: (SignInResponse?) -> Unit) {
+        Log.d("Authentication service","Called signin")
         authService.signIn(request).enqueue(object : Callback<SignInResponse> {
             override fun onResponse(call: Call<SignInResponse>, response: Response<SignInResponse>) {
                 if (response.isSuccessful) {
+                    Log.d("Authentication service","FSuscesfull signin")
                     callback(response.body())
                 } else {
+                    Log.d("Authentication service","Failed signin")
                     callback(null)
                 }
             }
-
             override fun onFailure(call: Call<SignInResponse>, t: Throwable) {
+                Log.d("Authentication service", "Error signin: ${t.message}")
                 callback(null)
             }
         })
